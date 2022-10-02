b7d2546dc51e412426763dd4a312bd5def8c1298
vanezza55 <masaichi2425@gmail.com>
2022-10-03 01:29:40 +0900

    j

--- RadiationAndWindWR/test.py
+++ RadiationAndWindWR/test.py
@@ -11,13 +11,15 @@ nr_ydata = int(meta.find('datadescription/nr_ydata').text)
 nr_zdata = int(meta.find('datadescription/nr_zdata').text)
 
 import numpy
-data = numpy.fromfile('surface/New Simulation_FX_2021-08-06_10.00.00.EDT','<f4')
+data = numpy.fromfile('atmosphere/New Simulation_AT_2021-08-06_10.00.00.EDT','<f4')
 
 cube = data.reshape((len(variable_names), nr_zdata, nr_ydata, nr_xdata))
 
-t = numpy.array(cube[8,0,:,:]).T
+t = numpy.array(cube[6,0,:,:])
 t[0,0]=100
 t[0,1]=100
+print(t.shape)
+print(nr_xdata)
 import matplotlib.pyplot as plt
 plt.imshow(t[:,:])
 plt.ylim(0,nr_ydata-1)
---
+++ RadiationAndWindWR/test2.py
@@ -0,0 +1,5 @@
+import numpy as np
+
+a= np.array([1,2,3,4,5,6])
+b= a.reshape(2,3)
+print(b)
